4bf4ada377a5d9754e06e8d537b9111dded83c3f
Дручек Михаил <mdruchek@yandex.ru>
2023-07-28 18:11:35 +0300

    added services.py

---
+++ frontend/services.py
@@ -0,0 +1,37 @@
+class AddingProductInCartService:
+    def adding_product(self):
+        pass
+
+    def delete_product(self):
+        pass
+
+    def change_number_products(self):
+        pass
+
+    def get_list_products(self):
+        pass
+
+    def get_number_products(self):
+        pass
+
+
+class AddingReviewProductService:
+    def adding_review(self):
+        pass
+
+    def get_list_reviews(self):
+        pass
+
+    def get_discount_on_cart(self):
+        pass
+
+    def get_number_reviews(self):
+        pass
+
+
+class IntegrationWithPaymentService:
+    def pay_order(self):
+        pass
+
+    def get_payment_status(self):
+        pass
